461c2c5207fed21ff698a4b493a2aceca030a2f2
Lucasanthiago <santoslblucas@gmail.com>
2024-11-22 15:47:54 -0300

    Initial Commit

---
+++ SnapDish/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SnapDish
+//
+//  Created by Lucas Santos on 22/11/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SnapDish/SnapDishApp.swift
@@ -0,0 +1,17 @@
+//
+//  SnapDishApp.swift
+//  SnapDish
+//
+//  Created by Lucas Santos on 22/11/24.
+//
+
+import SwiftUI
+
+@main
+struct SnapDishApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
